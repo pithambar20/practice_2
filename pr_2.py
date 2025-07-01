e077c1193ed19bc492999c4bb22a5bb1545ff975
pithambar20 <pithambar20@gmail.com>
2025-07-01 12:19:59 +0530

    factorial

---
+++ pr_2.py
@@ -0,0 +1,13 @@
+
+def factorial(n1):
+    if n1 < 0:
+        return "Factorial is not defined for negative numbers"
+    elif n1 == 0 or n1 == 1:
+        return 1
+    else:
+        result = 1
+        for i in range(2, n1 + 1):
+            result *= i
+        return result
+
+print(factorial(5))  # Output: 120
